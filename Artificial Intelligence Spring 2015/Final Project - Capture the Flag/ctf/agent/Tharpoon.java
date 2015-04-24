22a7bfb02ed10777d3cb5437a96455f694208252
ashley_tharp <ashley.tharp@gmail.com>
2015-04-23 21:14:47 -0500

    got the calculation of our base to work accurately but, the control flow is still not right, the program continues to keep trying to calculate base after we find it.  -Ash

--- Artificial Intelligence Spring 2015/Final Project - Capture the Flag/ctf/agent/Tharpoon.java
+++ Artificial Intelligence Spring 2015/Final Project - Capture the Flag/ctf/agent/Tharpoon.java
@@ -1,15 +1,20 @@
 package ctf.agent;
 
-
-import ctf.common.AgentEnvironment;
-import ctf.agent.Agent;
-
 import java.lang.System;
 import java.util.ArrayList;
-
+import java.lang.Math;
 import ctf.common.AgentAction;
+import ctf.common.AgentEnvironment;
+import ctf.agent.Agent;
+import ctf.common.AgentAction;
+import ctf.agent.TharpoonMap;
 
 public class Tharpoon extends Agent {
+
+    private static final int NORTH = 0;
+    private static final int SOUTH = 1;
+    private static final int EAST = 2;
+    private static final int WEST = 3;
 
     private static int MAX_NUM_AGENTS = 2;
 
@@ -17,7 +22,11 @@ public class Tharpoon extends Agent {
 
     private int agentNum;
 
-    private Boolean isBaseFound = false;
+    private static int[] spacesTravelledToBaseInPhaseOne = {1, 1};
+    private boolean haveBothAgentsFoundTheBase = false;
+
+    private static Boolean isBaseFound = false;
+    private static Boolean[] hasAgentIFoundBase = {false, false};
 
     // booleans describing direction of goal
     // goal is either enemy flag, or our base
@@ -31,186 +40,126 @@ public class Tharpoon extends Agent {
     private boolean obstEast;
     private boolean obstWest;
 
-    //map stuff
-    private static int knownMapHeight = 0;
-    private static int knownMapLength = 0;
+    private boolean enemyBaseNorth;
+    private boolean enemyBaseSouth;
+    private boolean enemyBaseEast;
+    private boolean enemyBaseWest;
 
-    //local map for keeping track of stuff until we make the global map
-    private ArrayList<ArrayList<String>> localMap;
+    //map stuff
+    private static int knownMapHeight = 2;
 
     //global map so all agents know about our environment
-    private ArrayList<ArrayList<String>> globalMap;
+    private static TharpoonMap globalMap;
 
     public Tharpoon(){
+        //This is for resetting everything back to initial values in the case that the map is restarted.
         if(numAgents == MAX_NUM_AGENTS){
             numAgents = 0;
-            private static int knownMapHeight = 0;
-            private static int knownMapLength = 0;
+            knownMapHeight = 2;
+            isBaseFound = false;
+            hasAgentIFoundBase[0] = false;
+            hasAgentIFoundBase[1] = false;
+            spacesTravelledToBaseInPhaseOne[0] =1;
+            spacesTravelledToBaseInPhaseOne[1] =1;
         }
         if(numAgents < MAX_NUM_AGENTS){
+            // This is for adding a new Agent
             numAgents++;
             agentNum = numAgents;
             System.out.println("added agent " + agentNum);
         }
+
+        hasAgentIFoundBase[agentNum - 1] = false;
     }
 
     // implements Agent.getMove() interface
     public int getMove( AgentEnvironment inEnvironment ) {
 
-        if(isBaseFound == false){
-            //at this point we know that the base is north or south of us because the agents are started on
+        if (haveBothAgentsFoundTheBase == false) {
+            //PHASE ONE: (Find our base , start a map) at this point we know that the base is north or south of us because the agents are started on
             //the same column as the base as far north or south as possible.
-
-            //check to see if the base is north of us
-            if(inEnvironment.isFlagNorth(inEnvironment.ENEMY_TEAM, false )){
-
-                //since it's north of us, check if it's immediately north of us
-                if(inEnvironment.isFlagNorth(inEnvironment.ENEMY_TEAM, true ) == false){
-                    //if the base was not immediately north of us then we have to get to it so we decide to go north
-                    
-                    return MOVE_NORTH;
-                }
-                else{
-                    //if it's immediately north of us we add our current knowledge to the global map
-
-                }
-            }
-
-        }
-
-
-
-        //if this agent doesn't have the enemy flag
-        if( !inEnvironment.hasFlag() ) {
-            // make goal the enemy flag
-            goalNorth = inEnvironment.isFlagNorth(
-                    inEnvironment.ENEMY_TEAM, false );
-
-            goalSouth = inEnvironment.isFlagSouth(
-                    inEnvironment.ENEMY_TEAM, false );
-
-            goalEast = inEnvironment.isFlagEast(
-                    inEnvironment.ENEMY_TEAM, false );
-
-            goalWest = inEnvironment.isFlagWest(
-                    inEnvironment.ENEMY_TEAM, false );
-        }
-        else {
-            // we have enemy flag.
-            // make goal our base
-            goalNorth = inEnvironment.isBaseNorth(
-                    inEnvironment.OUR_TEAM, false );
-
-            goalSouth = inEnvironment.isBaseSouth(
-                    inEnvironment.OUR_TEAM, false );
-
-            goalEast = inEnvironment.isBaseEast(
-                    inEnvironment.OUR_TEAM, false );
-
-            goalWest = inEnvironment.isBaseWest(
-                    inEnvironment.OUR_TEAM, false );
-        }
-
-        // now we have direction booleans for our goal
-
-        // check for immediate obstacles blocking our path
-        obstNorth = inEnvironment.isObstacleNorthImmediate();
-        obstSouth = inEnvironment.isObstacleSouthImmediate();
-        obstEast = inEnvironment.isObstacleEastImmediate();
-        obstWest = inEnvironment.isObstacleWestImmediate();
-
-
-        // if the goal is north only, and we're not blocked
-        if( goalNorth && ! goalEast && ! goalWest && !obstNorth ) {
-            // move north
-            return AgentAction.MOVE_NORTH;
-        }
-
-        // if goal both north and east
-        if( goalNorth && goalEast ) {
-            // pick north or east for move with 50/50 chance
-            if( Math.random() < 0.5 && !obstNorth ) {
-                return AgentAction.MOVE_NORTH;
-            }
-            if( !obstEast ) {
-                return AgentAction.MOVE_EAST;
-            }
-            if( !obstNorth ) {
-                return AgentAction.MOVE_NORTH;
-            }
-        }
-
-        // if goal both north and west
-        if( goalNorth && goalWest ) {
-            // pick north or west for move with 50/50 chance
-            if( Math.random() < 0.5 && !obstNorth ) {
-                return AgentAction.MOVE_NORTH;
-            }
-            if( !obstWest ) {
-                return AgentAction.MOVE_WEST;
-            }
-            if( !obstNorth ) {
-                return AgentAction.MOVE_NORTH;
-            }
-        }
-
-        // if the goal is south only, and we're not blocked
-        if( goalSouth && ! goalEast && ! goalWest && !obstSouth ) {
-            // move south
-            return AgentAction.MOVE_SOUTH;
-        }
-
-        // do same for southeast and southwest as for north versions
-        if( goalSouth && goalEast ) {
-            if( Math.random() < 0.5 && !obstSouth ) {
-                return AgentAction.MOVE_SOUTH;
-            }
-            if( !obstEast ) {
-                return AgentAction.MOVE_EAST;
-            }
-            if( !obstSouth ) {
-                return AgentAction.MOVE_SOUTH;
-            }
-        }
-
-        if( goalSouth && goalWest && !obstSouth ) {
-            if( Math.random() < 0.5 ) {
-                return AgentAction.MOVE_SOUTH;
-            }
-            if( !obstWest ) {
-                return AgentAction.MOVE_WEST;
-            }
-            if( !obstSouth ) {
-                return AgentAction.MOVE_SOUTH;
-            }
-        }
-
-        // if the goal is east only, and we're not blocked
-        if( goalEast && !obstEast ) {
-            return AgentAction.MOVE_EAST;
-        }
-
-        // if the goal is west only, and we're not blocked
-        if( goalWest && !obstWest ) {
-            return AgentAction.MOVE_WEST;
-        }
-
-        // otherwise, make any unblocked move
-        if( !obstNorth ) {
-            return AgentAction.MOVE_NORTH;
-        }
-        else if( !obstSouth ) {
-            return AgentAction.MOVE_SOUTH;
-        }
-        else if( !obstEast ) {
-            return AgentAction.MOVE_EAST;
-        }
-        else if( !obstWest ) {
-            return AgentAction.MOVE_WEST;
-        }
-        else {
-            // completely blocked!
+            return checkForBase(inEnvironment);
+        } else {
             return AgentAction.DO_NOTHING;
         }
+    }
+
+    public int checkForBase(AgentEnvironment inEnvironment){
+
+        Boolean baseNorth = inEnvironment.isBaseNorth(inEnvironment.OUR_TEAM, false);
+        Boolean baseSouth = inEnvironment.isBaseSouth(inEnvironment.OUR_TEAM, false);
+        Boolean baseImmediatelyNorth = inEnvironment.isBaseNorth(inEnvironment.OUR_TEAM, true);
+        Boolean baseImmediatelySouth = inEnvironment.isBaseSouth(inEnvironment.OUR_TEAM, true);
+
+        if((baseNorth && baseImmediatelyNorth) || (baseSouth && baseImmediatelySouth)){
+            //we know that the base is immediately above or below us
+            knownMapHeight++;
+            isBaseFound = true;
+            hasAgentIFoundBase[agentNum - 1] = true;
+            System.out.println("agent " + agentNum + " found the base");
+
+            //we cannot build the global map unless both agents have found the base.  In the case that the
+            //map has an even size one agent will find the base two turns before the other, and in the case
+            //that the map is odd size one agent will find the base one turn before the other,
+            //do nothing until the other agent finds the base
+            //The agent that finds the base first will do nothing until the other agent finds the base.
+            for(int i = 0; i < numAgents; i++){
+                //System.out.print("a" + i + " found base: " + hasAgentIFoundBase[i]);
+                System.out.println("but both agents have not found the base");
+                if(hasAgentIFoundBase[i] == false){
+                    return AgentAction.DO_NOTHING;
+                }
+            }System.out.println();
+
+            haveBothAgentsFoundTheBase = true;
+            System.out.println("both agents have now found the base");
+            return caseBothAgentsHaveFoundTheBase(inEnvironment);
+        }
+        else {
+            //We know that the base is north or south of us, and that we are not immediately next to it
+            knownMapHeight++;
+            spacesTravelledToBaseInPhaseOne[agentNum-1]++;
+            System.out.println("agent " + agentNum + " moved " + ((baseNorth) ? ("north") : ("south")));
+            return (baseNorth) ? (AgentAction.MOVE_NORTH) : (AgentAction.MOVE_SOUTH);
+        }
+    }
+
+    public int caseBothAgentsHaveFoundTheBase(AgentEnvironment inEnvironment){
+        System.out.println("boop");
+        globalMap = new TharpoonMap(knownMapHeight - 1);
+        int ourBaseX;
+        int ourBaseY;
+
+        //now we figure out what side of the map we are on by seeing if the enemy base is east or west
+        enemyBaseEast = inEnvironment.isBaseEast(inEnvironment.ENEMY_TEAM, false);
+        enemyBaseWest = inEnvironment.isBaseWest(inEnvironment.ENEMY_TEAM, false);
+
+        //if our base is on the east, our base x coordinate is 0,
+        //if on the west side, our base x coordinate is the size of the map - 1
+        ourBaseX = (enemyBaseEast) ? (knownMapHeight -1) : (0);
+
+        int whichAgentFoundBaseFirst;
+        if(spacesTravelledToBaseInPhaseOne[0] == Math.min(spacesTravelledToBaseInPhaseOne[0], spacesTravelledToBaseInPhaseOne[1])){
+            whichAgentFoundBaseFirst = 1;
+        }else{
+            whichAgentFoundBaseFirst = 2;
+        }
+        Boolean isAgentThatFoundBaseFirstNorthOfBase;
+        if(agentNum == whichAgentFoundBaseFirst){
+            isAgentThatFoundBaseFirstNorthOfBase = inEnvironment.isBaseSouth(inEnvironment.OUR_TEAM, false);
+        }else{
+            isAgentThatFoundBaseFirstNorthOfBase = !inEnvironment.isBaseSouth(inEnvironment.OUR_TEAM, false);
+        }
+
+        if(isAgentThatFoundBaseFirstNorthOfBase){
+            ourBaseY = spacesTravelledToBaseInPhaseOne[whichAgentFoundBaseFirst];
+        }
+        else{
+            ourBaseY = knownMapHeight - spacesTravelledToBaseInPhaseOne[whichAgentFoundBaseFirst - 1] - 1;
+        }
+
+        System.out.println("Our base was calculated to be: (" + ourBaseX + ", " + ourBaseY + ")");
+
+        return AgentAction.DO_NOTHING;
     }
 }
