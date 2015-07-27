4232ba2bba4d35a08255958b45bbb40aa2539804
Ashley Tharp <ashley.tharp@gmail.com>
2015-07-27 13:22:05 -0500

    cleanup

--- School - Comp Sci/Artificial Intelligence Spring 2015/Assn 2 - Backtracking Search/version 1/Node.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'ashley'
-
-
-class Node:
-        """ This node class is specific to CSPs"""
-
-        def __init__(self, parentArg = None):
-            self.parent = parentArg
-            self.assignment = {}
