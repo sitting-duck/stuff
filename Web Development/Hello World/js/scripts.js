dc5fbf45b095c9082fc558cbbb64762abbeb8164
ashley_tharp <ashley.tharp@gmail.com>
2015-10-30 13:27:03 -0500

    had to delete and re add web development folder because I added within the doler first. oops.

--- Web Development/Hello World/js/scripts.js
+++ Web Development/Hello World/js/scripts.js
@@ -29,12 +29,39 @@ $(document).ready(function () {
 		//console.log(hex_color_string);
 		
 		//create an html string from that string
-		var open_p = '<p>';  var close_p = '</p>'		
-		var html_hex_string = open_p.concat(hex_color_string).concat(close_p);
-		console.log(html_hex_string);
+		//var open_p = '<p>';  var close_p = '</p>'		
+		//var html_hex_string = open_p.concat(hex_color_string).concat(close_p);
+		//console.log(html_hex_string);
 		
 		//for testing for now display the hex string below hello world
-		$(html_hex_string).appendTo($hw);
+		//$(html_hex_string).appendTo($hw);
+		
+		//check if the string is a valid hex color value
+		//test our function call
+		var hex_string_was_valid = isValidHexColorString(hex_color_string);
+		console.log(hex_string_was_valid);
+		
+		if(hex_string_was_valid){
+			
+			var style_sheet = getStyleSheet();
+			
+			style_sheet.body.backgroud-color =  String(hex_color_string));
+			
+		}
 		
 	});
 });
+
+function isValidHexColorString(inputString){
+
+	//todo: test this regex for all hex values
+	return /(^#?[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(inputString);
+	
+}
+
+function getStyleSheet(){
+
+	//at the moment there is only one style sheet
+	return document.stylesheets[0];
+
+}
