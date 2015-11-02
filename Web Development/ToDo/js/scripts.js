3b11e75bbc43f28600bfa08f612dc921d4e15868
sitting-duck <=>
2015-11-01 22:39:32 -0600

    got the items to add to the list.

--- Web Development/ToDo/js/scripts.js
+++ Web Development/ToDo/js/scripts.js
@@ -3,10 +3,22 @@ $(document).ready (function () {
 	//callback when add button is clicked
 	$('#add_button').on('click', function (event) {		
 	
+		//grab the text from the to do input field
 		var to_do_item_text = $('#input_field').val();
+
+		//clear the input field
+		$('#input_field')[0].reset();
 		
-		console.log(to_do_item_text);
+		//construct the list item
+		var list_item = "<div>" +
+		"<li>" + to_do_item_text +
+		"<a href='#' class='button' id='x_button'>X</a>" + "</li>" +
+		"</div>";
 	
+		//append the item to the list
+		$('#list').append(list_item);
+
+
 	});
 
 });
