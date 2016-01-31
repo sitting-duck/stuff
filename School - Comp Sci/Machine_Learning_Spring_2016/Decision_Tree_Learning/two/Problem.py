0113f374716fa4d224edcf2da0ed5dca27d06743
Ashley Tharp <Ashley Tharp>
2016-01-31 10:42:57 -0600

    finished converting all the float() and round() functions with my own prec() function that uses Decimal and rounds to 3 places after the decimal by default. Now to check and see whether this has enabled me to reduce my margin of error.

--- School - Comp Sci/Machine_Learning_Spring_2016/Decision_Tree_Learning/two/Problem.py
+++ School - Comp Sci/Machine_Learning_Spring_2016/Decision_Tree_Learning/two/Problem.py
@@ -14,7 +14,7 @@ class Problem:
     decision_tree = Decision_Tree()
 
     def __init__(self):
-        print str(getcontext())
+        pass
 
     def create_decision_tree(self):
 
@@ -34,19 +34,19 @@ class Problem:
             if current_information_gain > current_best_information_gain:
                 current_best_information_gain = current_information_gain
                 current_best_category = category
-        return round(current_best_category, 3)
+        return self.prec(current_best_category, 3)
 
     # information gain is a metric to measure the amount of information gained if we split the tree at this category
     def calculate_information_gain_for_category(self, category, parent_entropy, training_set):
         training_set_for_category_entropy = self.calculate_entropy_for_category(category, training_set)
-        information_gain = float(parent_entropy) - float(training_set_for_category_entropy)
-        return round(information_gain, 3)
+        information_gain = self.prec(parent_entropy) - self.prec(training_set_for_category_entropy)
+        return self.prec(information_gain)
 
     # information gain is a metric to measure the amount of information gained if we split the tree at this category
     def calculate_information_gain_for_category_for_undefined_root(self, category, training_set):
         training_set_entropy = self.calculate_entropy_for_training_set(training_set)
         information_gain = self.calculate_information_gain_for_category(category, training_set_entropy, training_set)
-        return round(information_gain, 3)
+        return self.prec(information_gain)
 
     def calculate_entropy_for_training_set(self, training_set):
 
@@ -56,9 +56,9 @@ class Problem:
         num_class_types = len(class_types)
         for i in range(0, num_class_types):
             num_current_type = self.training_set.get_number_of_training_examples_of_class_type(class_types[i], training_set)
-            Pi = float(num_current_type / num_training_examples)
-            sum -= float(Pi)*(float(math.log(Pi, 2)))
-        return round(sum, 3)
+            Pi = self.prec(num_current_type / num_training_examples)
+            sum -= self.prec(Pi)*(self.prec(math.log(Pi, 2)))
+        return self.prec(sum, 3)
 
     def calculate_entropy_for_category(self, category, training_set):
 
@@ -67,9 +67,9 @@ class Problem:
         for attribute in attributes:
             num_current_attribute = self.training_set.get_number_of_training_examples_for_attribute(category, attribute, training_set)
             num_training_examples = self.training_set.get_number_training_examples(training_set)
-            Pi = float(num_current_attribute / num_training_examples)
+            Pi = self.prec(num_current_attribute / num_training_examples)
             Hi = self.calculate_entropy_for_attribute(category, attribute, training_set)
-            sum += float( Pi * Hi)
+            sum += self.prec( Pi * Hi)
         return sum
 
     def calculate_entropy_for_attribute(self, category, attribute, training_set):
@@ -81,16 +81,16 @@ class Problem:
         num_training_examples_for_current_attribute = self.training_set.get_number_training_examples(reduced_training_set)
         for current_type, num_current_type in class_type_frequency.iteritems():
 
-            Pi = float(num_current_type)/float(num_training_examples_for_current_attribute)
+            Pi = self.prec(num_current_type)/self.prec(num_training_examples_for_current_attribute)
 
             # for the edge case of log(0) (convenient loophole for computing entropy)
             if Pi == 0.0:
                 sum = 0.0
             else:
-                sum -= float(Pi)*(float(round(math.log(Pi, 2), 3)))
-            sum = round(sum, 3)
+                sum -= self.prec(Pi) * self.prec(math.log(Pi, 2))
+            sum = self.prec(sum, 3)
 
-        return round(sum, 3)
+        return self.prec(sum, 3)
 
     def get_categories(self):
         return self.training_set.get_categories()
