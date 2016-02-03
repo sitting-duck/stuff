362a5354b53996c96ce095219692fc8500080592
Ashley <ashley.tharp@gmail.com>
2016-02-02 20:09:20 -0600

    finished selection sort

--- interview_questions_practice/sorting/python/insertion_sort/main.py
+++ interview_questions_practice/sorting/python/insertion_sort/main.py
@@ -5,6 +5,21 @@ __author__ = "Ashley Tharp"
 import random
 
 def main():
+    """
+
+    Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. However, insertion sort provides several advantages:
+
+    Simple implementation: Bentley shows a three-line C version, and a five-line optimized version[1]:116
+    Efficient for (quite) small data sets, much like other quadratic sorting algorithms
+    More efficient in practice than most other simple quadratic (i.e., O(n2)) algorithms such as selection sort or bubble sort
+    Adaptive, i.e., efficient for data sets that are already substantially sorted: the time complexity is O(nk) when each element in the input is no more than k places away from its sorted position
+    Stable; i.e., does not change the relative order of elements with equal keys
+    In-place; i.e., only requires a constant amount O(1) of additional memory space
+    Online; i.e., can sort a list as it receives it
+
+    When people manually sort cards in a bridge hand, most use a method that is similar to insertion sort
+
+    """
 
     original_set = random.sample(range(1, 11), 10)
 
@@ -14,8 +29,7 @@ def main():
 
     print "original set: " + str(original_set)
 
-    print "sorted_set: " + str(sorted_set)
-
+    print "sorted_set:   " + str(sorted_set)
 
 def insertion_sort(set, comparator = None):
     # this function assumes that arrays are zero based
