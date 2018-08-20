7bfb07035b367801d5eb6a2c3fca8b5d8ef39174
AshleyTharpTL <ashley.tharp@topazlabs.com>
2018-08-20 12:44:41 -0800

    I did manage to get it to act more like a proper image gallery by hiding all the elements at the beginning of each call chain.
    There is still the issue where all the images are in a tower and have to
    be disappeared before the thing will start acting like a proper gallery.

--- web_dev/jquery_photo_gallery/index.js
+++ web_dev/jquery_photo_gallery/index.js
@@ -1,10 +1,17 @@
+
+// gt means: Select all elements at an index greater than index within the matched set.
+// so here it appears we are hiding all the elements inside the slideshow.
 $("#slideshow > div:gt(0)").hide();
 
+
+// It seems to be the case that each of these function calls returns 
+// the jquery object, which contains the entire DOM.
 setInterval(function() {
   $('#slideshow > div:first')
+  	.hide()
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-}, 3000);
+}, 1000);
