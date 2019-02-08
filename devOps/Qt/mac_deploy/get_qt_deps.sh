f4e4b3c2a09e5f2cbbc214c76942f1829ceffb4b
sitting-duck <ashley.tharp@gmail.com>
2019-02-08 09:56:53 -0600

    comments and explanation of the raison d'etre for the get_deps script.
    Really this mission was neglected, but it would have saved me time if I had kept doing this from the start.

--- devOps/Qt/mac_deploy/get_qt_deps.sh
+++ devOps/Qt/mac_deploy/get_qt_deps.sh
@@ -3,7 +3,22 @@
 ##
 ## NOTE: WE ARE COPYING INTO THE BUNDLE IN MAC_SETUP
 ##
-APP="/Users/Shared/Jenkins/Home/workspace/Program/mac_setup/Test Program.app"
+
+# I write a script like this because copying all these dependencies manually with a mouse is painful. At any moment, I could be forced to create a new
+# deploy environment on another machine, if, for example, a developer introduces a dependency that makes my machine no longer appropriate for deployment, 
+# perhaps MacOS has released their next version, and I cannot update this machine to the next OS because Apple says my hardwarenew  will not support the new OS, 
+# my machine runs out of space and I am unwilling to delete any deployment related projects, so I need to just start building new projects on a new machine, 
+# this script will just fetch what we need for a QT app on the mac. 
+
+# Every time you update which version of Qt you are using, you need to update the QTDIR 
+# variable and possibly even write a new slightly modified version of this script. With every new version of Qt, some packages will be added, some deleted, 
+# renamed or moved around. You can expect that your script will be able to mostly stay the same, but probably one major thing has been renamed. Keeping up a little
+# bit with what is new in this Qt version may help you, but isn't necessarily required.
+
+# Ideally you'll have a different version of this script so that at any time anyone can whip up a new deploy environment on any machine for whatever version of Qt 
+# they need
+
+APP="/Users/Shared/Jenkins/Home/workspace/Program/mac_setup/Test Program.app
 PLUGINS_DIR="$APP/Contents/PlugIns"
 QTDIR="/Users/macbuild/Qt/5.6.3/clang_64"
 QTIFW="/Users/Shared/Jenkins/Home/workspace/Program/mac_setup/Program/"
@@ -16,6 +31,7 @@ cp "$QTDIR"/plugins/audio/libqtaudio_coreaudio.dylib "$PLUGINS_DIR"/audio/
 cp "$QTDIR"/plugins/bearer/libqcorewlanbearer.dylib "$PLUGINS_DIR"/bearer/
 cp "$QTDIR"/plugins/bearer/libqgenericbearer.dylib "$PLUGINS_DIR"/bearer/
 
+# PLUGINS_DIR refers to Qt plugins, not your developed plugins. imageformats are a Qt plugin
 # I am not sure what this dds type image is, it seems to be present in some Qt versions and not in others
 #cp "$QTDIR"/plugins/imageformats/libqdds.dylib "$PLUGINS_DIR"/imageformats/
 cp "$QTDIR"/plugins/imageformats/libqgif.dylib "$PLUGINS_DIR"/imageformats/
@@ -35,22 +51,6 @@ cp "$QTDIR"/plugins/mediaservice/libqtmedia_audioengine.dylib "$PLUGINS_DIR"/med
 cp "$QTDIR"/plugins/platforms/libqcocoa.dylib  "$PLUGINS_DIR"/platforms/
 
 cp "$QTDIR"/plugins/printsupport/libcocoaprintersupport.dylib  "$PLUGINS_DIR"/printsupport/
-
-## the plugins "quick" dir apparently doesn't exist for Qt 5.6.3
-# cp "$QTDIR"/plugins/quick/libdeclarative_multimedia.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libdialogplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libdialogsprivateplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libmodelsplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqmlfolderlistmodelplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqmlsettingsplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqquicklayoutsplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqtgraphicaleffectsprivate.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqtquick2plugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqtquickcontrolsplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqtquickextrasflatplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libqtquickextrasplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libwidgetsplugin.dylib "$PLUGINS_DIR"/quick/
-# cp "$QTDIR"/plugins/quick/libwindowplugin.dylib "$PLUGINS_DIR"/quick/
 
 cp "$QTDIR"/plugins/sqldrivers/libqsqlite.dylib "$PLUGINS_DIR"/sqldrivers/
 cp "$QTDIR"/plugins/sqldrivers/libqsqlmysql.dylib "$PLUGINS_DIR"/sqldrivers/
@@ -76,27 +76,6 @@ rm -rf "$QTIFW"/contents-beta/qt/Test\ Program.app/Contents/Frameworks/QtQuickCo
 rm -rf "$QTIFW"/contents-beta/qt/Test\ Program.app/Contents/Frameworks/QtQuickParticles.framework
 rm -rf "$QTIFW"/contents-beta/qt/Test\ Program.app/Contents/Frameworks/QtQuickTemplates2.framework
 rm -rf "$QTIFW"/contents-beta/qt/Test\ Program.app/Contents/Frameworks/QtXmlPatterns.framework
-
-# DELETE THE APPLICATION OSX LIBS FROM MACOS
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libalgorithms.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libalgorithms.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libalgorithms.*.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libalgorithms.*.*.*.dylib
-
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimpbase.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimpbase.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimpbase.*.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimpbase.*.*.*.dylib
-
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimprocs.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimprocs.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimprocs.*.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/libimprocs.*.*.*.dylib
-
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/librender.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/librender.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/librender.*.*.dylib
-rm -rf "$QTIFW"/contents-beta/tlsw/Test\ Program.app/Contents/MacOs/librender.*.*.*.dylib
 
 ##
 ## DESTROY ALL DEBUG FILE BLOAT DRAGGED IN BY MACDEPLOYQT (why would anyone deploy debug code wtf?)
