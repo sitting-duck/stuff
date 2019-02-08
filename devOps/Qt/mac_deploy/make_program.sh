71c1767e34432776491c229445433bb1d3d88ec9
sitting-duck <ashley.tharp@gmail.com>
2019-02-08 10:18:48 -0600

    now these are finally some good looking notes, damn.

--- devOps/Qt/mac_deploy/make_program.sh
+++ devOps/Qt/mac_deploy/make_program.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/sh
 
 APP_NAME="Test Program"
@@ -21,19 +19,20 @@ cp /usr/local/lib_old/*.*.*.*.dylib /Projects/mycompany/myprogram/mac_setup/"${A
 cp ./Contents/Info.plist ./"${APP_NAME}".app/Contents/
 cp ./Contents/Resources/TestProgram.icns ./"${APP_NAME}".app/Contents/Resources/
 cp ./Contents/Resources/TestProgrambeta.icns ./"${APP_NAME}".app/Contents/Resources/
-cp -R ./Contents/Resources/"${APP_NAME_NO_SPACE}".plugin ./"${APP_NAME}".app/Contents/Resources/
-cp ./Contents/Resources/"${APP_NAME_NO_SPACE}".lrtemplate ./"${APP_NAME}".app/Contents/Resources/
+cp -R ./Contents/Resources/"${APP_NAME_NO_SPACE}".plugin ./"${APP_NAME}".app/Contents/Resources/ # Your app ships as/with a Photoshop Plugin
+cp ./Contents/Resources/"${APP_NAME_NO_SPACE}".lrtemplate ./"${APP_NAME}".app/Contents/Resources # Your app ships as/with a Lightroom Plugin
 
 # macdeployqt uses otool to grab the list of dependencies your application has. It then searches the otool output of every dependency of every dependency
 # and copies every Qt dependency from the Qt directory
-
 cd ~/Qt/5.6/clang_64/bin/
 ~/Qt/5.6/clang_64/bin/macdeployqt /Projects/mycompany/myprogram/mac_setup/"${APP_NAME}".app -qmldir=/Projects/mycompany/myprogram/
-~/Qt/5.6/clang_64/bin/macdeployqt /Projects/mycompany/myprogram/mac_setup/"${APP_NAME}".app -qmldir=/Projects/mycompany/myprogram/mac_setup/"${APP_NAME}".app/Contents/MacOS
-cd /Projects/mycompany/myprogram/mac_setup/
 
+# Ideally you want all your company's program binaries in MacOS, and all your third party library depenedencies in Frameworks
+# also, developers will often just hand you the entire build of the library they are using, and may have built it in "build everything mode"
+# this is fine because their motivation is just to get a working prototype, it is your job to do cleanup and deploy only what is needed and nothing more
+cd /Projects/mycompany/myprogram/mac_setup/
 mv "${APP_NAME}".app/Contents/MacOS/*.dylib "${APP_NAME}".app/Contents/Frameworks 
-rm "${APP_NAME}".app/Contents/MacOS/*.a
+rm "${APP_NAME}".app/Contents/MacOS/*.a # these .a files are static libs. Your app may actually use these, but oftentimes not, in that case remove them
 
 # copy in needed qml binaries
 cp -R qml_56/* "${APP_NAME}".app/Contents/MacOS
