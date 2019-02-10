dfd62ca49ce187cb341d0e332ea3908e384e58d9
sitting-duck <ashley.tharp@gmail.com>
2019-02-10 14:39:41 -0600

    notes concerning cross entropy

--- data_science/tensorflow/oreilly_learning_tensorflow/classifying_MNIST_handwritten_digits_with_softmax_regression/test.py
+++ data_science/tensorflow/oreilly_learning_tensorflow/classifying_MNIST_handwritten_digits_with_softmax_regression/test.py
@@ -31,4 +31,29 @@ W = tf.Variable(tf.zeros([784, 10]))
 y_true = tf.placeholder(tf.float32, [None, 10])
 y_pred = tf.matmul(x, W)
 
+# Before getting to cross entropy, let's do a little refresher on entropy, a measurement of how certain an event is. It describes how unpredictable that
+# probability distribution is. entropy = 1, truly random, entropy = 0, data is truly uniform and so we can always predicts its value.
+#
+# so now, cross entropy, is just the average message length, if there are 8 options, we can make a bit string to rep each of these states with 3 bits, 
+# because 2^3 == 8
+#
+# for cross entropy, we do sum of pilog(qi), instead of the normal entropy equation, sumo f pilog(pi). Here, qi, is the predicted probability, and pi
+# is the actual probability. 
+
+# cross entropy, also known as log loss, measures the performance of a classification model, whose output is a probability between 0 and 1.
+# log loss penalizes both types of errors, but especially those that are both confident and wrong. It is a common loss function for training classifiers.
+#
+# Code
+#
+#def CrossEntropy(yHat, y):
+#    if y == 1:
+#      return -log(yHat)
+#    else:
+#      return -log(1 - yHat)
+#
+# In information theory, the cross entropy between two probability distributions p and q over the same
+# underlying set of events measures the average number of bits needed to identify an event drawn from the set, if a coding scheme is used that
+# is optimized for an "artificial" probability distribution q, rather than the "true" distribution p. 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits
+
+
