5db8119af3303cfbc48ac17d19196bf60d7a31e8
sitting-duck <ashley.tharp@gmail.com>
2019-12-03 07:30:37 -0600

    for reasons I do not understand, random.shuffle is not behaving the way I expect it should and I am about to lose my mind.

--- ai/tensorflow/dataset/file_tools.py
+++ ai/tensorflow/dataset/file_tools.py
@@ -1,4 +1,5 @@
-import os
+import os     # Python file path utils
+import random # for shuffling an array of file paths
 
 # paths: an array of filepath strings eg. "./images"
 # valid_file_extensions: an array of valid file extensions eg. ".tif", ".jpg" etc.
@@ -12,4 +13,11 @@ def files(paths, valid_file_extensions):
                     outlist.append( os.path.join(root, filename) )
     print('outlist: ', outlist)
     return outlist
-            
+
+# paths: an array of filepath strings eg. "./images"
+# valid_file_extensions: an array of valid file extensions eg. ".tif", ".jpg" etc.
+def files_random(paths, valid_file_extensions):
+    got_files = files(paths, valid_file_extensions)
+    random.shuffle(got_files)
+    random.shuffle(got_files)
+    return files 
