0d225c34b171019c3d7bf028c3838ac62700c2a3
sitting-duck <ashley.tharp@gmail.com>
2019-12-03 06:47:35 -0600

    can confirm that the fucnction works for recursive directory search, next item is to make sure that it works for multiple directories so that I can keep adding to my Tensorflow dataset as I find I new sources of data.
    Ideally, I want to be able to just add a folder at will anytime I complete a new data collection mission

--- ai/tensorflow/dataset/main.py
+++ ai/tensorflow/dataset/main.py
@@ -5,6 +5,6 @@ import tensorlayer as tl
 import file_tools as ft
 
 paths = ["images"]
-valid_extensions = [".tiff", ".tif", ".png", ".jpg", ".jpeg"]
+valid_extensions = [".tiff", ".tif", ".png", ".jpg", ".jpeg", ".webp"]
 filenames = ft.files(paths, valid_extensions) 
 dataset = tf.data.Dataset.from_tensor_slices((filenames))
